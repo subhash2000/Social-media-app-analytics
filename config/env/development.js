ce1303e35db26f748fabbfc784ee1572e8ff2d8f
Subhash0310 <subhash.yadav@postman.com>
2024-01-16 17:02:46 +0530

    Commit for analytics app

--- config/env/development.js
+++ config/env/development.js
@@ -1,34 +1,33 @@
+/* eslint-disable no-process-env */
 /**
  * @fileOverview
  * The development environment defaults are stored in this file. Any additional configuration changes can be
  * done using environment variables used using `env-lift`.
  */
+require('dotenv').config();
 
- module.exports = require('env-lift').load('bruke', {
-  port: 1337,
 
-  env: 'development',
-
+module.exports = require('env-lift').load('Social-media-app-analytics', {
+  port: process.env.PORT || 3000,
+  env: process.env.ENV || 'development',
   datastores: {
-    database: 'bruke',
-    port: 3306,
+    database: process.env.DB_NAME || 'social_media_analytics',
+    port: process.env.DB_PORT || 3306,
     pool: {
-      // override other connection pool properties here if needed.
       max: 20,
       acquire: 60000
     },
     mysql: {
-      host: 'localhost',
-      user: 'root',
-      password: ''
+      host: process.env.MYSQL_HOST || 'localhost',
+      user: process.env.MYSQL_USER || 'root',
+      password: process.env.MYSQL_PASSWORD || ''
     },
     mysqlR: {
-      host: 'localhost',
-      user: 'root',
-      password: ''
+      host: process.env.MYSQLR_HOST || 'localhost',
+      user: process.env.MYSQLR_USER || 'root',
+      password: process.env.MYSQLR_PASSWORD || ''
     }
   },
-
 
   // these will be directly overridden with
   // the base sequelize operations.
--- config/routes.js
+++ config/routes.js
@@ -1,33 +1,50 @@
-const HealthController = require('../api/controllers/HealthController');
+const HealthController = require('../api/controllers/HealthController'),
+  PostsController = require('../api/controllers/PostsController');
 
-  /**
+/**
    *
    * @param {String} name - unique scehma validation file name
    * @returns {Object} schema object
    */
-  getSchema = function (name) {
-    let schema = {};
+const getSchema = function (name) {
+  let schema = {};
 
-    try {
-      // eslint-disable-next-line security/detect-non-literal-require
-      schema = require(`./schemas/${name}`);
-    }
-    catch (e) {
-      // Todo - Add error handling
-    }
+  try {
+    // eslint-disable-next-line security/detect-non-literal-require
+    schema = require(`./schemas/${name}`);
+  }
+  catch (e) {
+    // Todo - Add error handling
+  }
 
-    return schema;
-  };
+  return schema;
+};
 
 module.exports = function (fastify, opts, done) {
   // routes for health controller
   fastify.get('/', {
+    config: {
+      rateLimit: false
+    },
     handler: HealthController.status
   });
 
   fastify.get('/knockknock', {
+    config: {
+      rateLimit: false
+    },
     handler: HealthController.check,
     schema: getSchema('healthCheck')
+  });
+
+  // routes for posts controller
+  fastify.post('/api/v1/posts', {
+    handler: PostsController.createPosts,
+    schema: getSchema('createPosts')
+  });
+
+  fastify.get('/api/v1/posts/:uniqueIdentifier/analysis', {
+    handler: PostsController.findByUniqueIdentifier
   });
 
   done();
